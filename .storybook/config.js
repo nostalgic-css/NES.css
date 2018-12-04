fd183a65788aff8cae20728677bc0b0b5a8c25d3
Abdallah Samman <sammanabdallah@gmail.com>
2018-12-04 17:13:03 +0200

    Add pre-commit hook to eslint javascript code; import nes.min.css globaly in stories;

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,7 @@
 import { configure } from '@storybook/html';
 
+import '../css/nes.min.css'
+
 // automatically import all files ending in *.stories.js
 const req = require.context('../docs', true, /.stories.js$/);
 function loadStories() {
