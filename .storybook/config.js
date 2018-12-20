560d128efc4a2ddfd2ea201c677d0be3fc5a617c
Trezy <tre@trezy.com>
2018-12-20 13:25:52 -0600

    feat(storybook): Add backgrounds Storybook
    I've added the Storybook backgrounds addon which allows users viewing the storybook to change the
    background behind components, which will make it easier to see how the main NES.css theme looks in
    various contexts.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,8 @@
-import { configure } from '@storybook/html'; // eslint-disable-line import/no-extraneous-dependencies
+import {
+  addDecorator,
+  configure,
+} from '@storybook/html'; // eslint-disable-line import/no-extraneous-dependencies
+import { withBackgrounds } from '@storybook/addon-backgrounds'; // eslint-disable-line import/no-extraneous-dependencies
 
 import '../scss/nes.scss'; // eslint-disable-line import/no-unresolved
 
@@ -7,5 +11,18 @@ const req = require.context('../docs', true, /.stories.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addDecorator(
+  withBackgrounds([
+    { name: 'Blue', value: 'blue'},
+    { name: 'Green', value: 'green' },
+    { name: 'Yellow', value: 'yellow' },
+    { name: 'Orange', value: 'orange' },
+    { name: 'Red', value: 'red' },
+    { name: 'Purple', value: 'purple' },
+    { name: 'Black', value: 'black' },
+    { name: 'White', value: 'white', default: true },
+  ])
+);
 
 configure(loadStories, module);
