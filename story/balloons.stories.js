18e6c476fc5e2e3fd29af9d1a2a46fec899aa7ef
Kaylum Lally <kaylum@hikaylum.me>
2019-10-11 19:24:29 +0100

    feat(component-balloons): Dark Mode - Balloons
    Implementation so balloons can now have `is-dark`
    
    re #345

--- story/balloons.stories.js
+++ story/balloons.stories.js
@@ -1,16 +1,18 @@
 import { storiesOf } from '@storybook/html'; // eslint-disable-line import/no-extraneous-dependencies
 import { // eslint-disable-line import/no-extraneous-dependencies
-  withKnobs, radios,
+  withKnobs, boolean, radios,
 } from '@storybook/addon-knobs';
 
 const stories = storiesOf('Ballons', module);
 stories.addDecorator(withKnobs);
 
 stories.add('balloon', () => {
-  const selectedClass = radios('direction', {
+  const isDark = boolean('is-dark', false) ? 'is-dark' : '';
+  const alignment = radios('direction', {
     default: '',
     'from-left': 'from-left',
     'from-right': 'from-right',
   }, '');
-  return `<div class="nes-balloon ${selectedClass}"> <p>Hello NES.css</p> </div>`;
+  const selectedClasses = [isDark, alignment];
+  return `<div class="nes-balloon ${selectedClasses.join(' ')}"> <p>Hello NES.css</p> </div>`;
 });
