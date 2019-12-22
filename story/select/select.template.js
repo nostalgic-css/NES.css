6b6f68ee8ce464addb76a0b6f513bacf6a5205e2
HiKaylum <kaylum@hikaylum.me>
2019-12-22 15:41:33 +0000

    fix(select): fixed select state styling
    select when `is-dark` can now have `is-error`, `is-success` and `is-warning`
    
    #384

--- story/select/select.template.js
+++ story/select/select.template.js
@@ -1,16 +1,23 @@
-import { select } from '@storybook/addon-knobs';
+import { boolean, select } from '@storybook/addon-knobs';
+import classNames from 'classnames';
 
 import sharedOptions from '../_helpers/shared';
 
 export default () => {
-  const selectedClass = select('class', {
+  const isDark = boolean('is-dark', false);
+  const selectOptions = select('class', {
     default: '',
     ...sharedOptions,
-    'is-dark': 'is-dark',
   }, '');
 
+  const selectedClasses = classNames(
+    'nes-select',
+    selectOptions,
+    { 'is-dark': isDark },
+  );
+
   return `
-    <div class="nes-select ${selectedClass}">
+    <div class="${selectedClasses}">
       <select required>
         <option value="" disabled selected hidden>Select...</option>
         <option value="0">To be</option>
