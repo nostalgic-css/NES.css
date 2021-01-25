71974a186c34e98fd4dd1429ec225c6067a70c4d
besasam <besa@uber.space>
2021-01-25 10:34:23 +0100

    feat: Add "is-pixelated" class for avatars with styles for all browsers

--- story/avatars/avatars.template.js
+++ story/avatars/avatars.template.js
@@ -3,6 +3,7 @@ import classNames from 'classnames';
 
 export default () => {
   const isRounded = boolean('is-rounded', false);
+  const isPixelated = boolean('is-pixelated', false);
   const avatarSize = select('Avatar Classes', {
     default: '',
     'is-small': 'is-small',
@@ -15,12 +16,12 @@ export default () => {
     avatarSize,
     {
       'is-rounded': isRounded,
+      'is-pixelated': isPixelated,
     },
   );
 
   return `
     <img src="http://www.gravatar.com/avatar?s=15" class="${avatarClasses}"
-      alt="Gravatar Image Example"
-      style="image-rendering: pixelated;">
+      alt="Gravatar Image Example">
   `;
 };
